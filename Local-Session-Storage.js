ad5370765f285503631ca1c3bd7d25d1d278d6d8
santosh_bcs <santosh.chaudhary@bcstechnology.com.au>
2021-11-07 14:35:50 +0530

    first comit

---
+++ Local-Session-Storage.js
@@ -0,0 +1,27 @@
+document.querySelector('#myFileInput').addEventListener('change', function() {
+    //console.log(this.files);
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+        //console.log(reader.result)
+        localStorage.setItem('recent-image', reader.result)
+    })
+    reader.readAsDataURL(this.files[0]);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const recentImageDataUrl = localStorage.getItem('recent-image')
+    if(recentImageDataUrl) {
+        document.querySelector('#imgPreview').setAttribute('src', recentImageDataUrl)
+    }
+})
+
+
+
+const person = {
+    name: "Obaseki Nosa",
+    location: "Lagos",
+}
+
+window.localStorage.setItem('user', JSON.stringify(person));
+
+console.log(localStorage.getItem('user'))
